4dd6cbdec7ae778dfefa80dd6d1a3b8858064bb7
Oleg Filippov <oleg@polbox.tv>
2022-11-10 14:35:46 +0200

    Update VLC

--- travis_before_install.sh
+++ travis_before_install.sh
@@ -6,7 +6,7 @@ then
 fi
 
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-  curl -L -o ./deps/vlc-$VLC_VER.dmg http://get.videolan.org/vlc/$VLC_VER/macosx/vlc-$VLC_VER.dmg
-  hdiutil mount ./deps/vlc-$VLC_VER.dmg
+  curl -L -o ./deps/vlc-$VLC_VER.dmg http://get.videolan.org/vlc/$VLC_VER/macosx/vlc-$VLC_VER-intel64.dmg
+  hdiutil mount ./deps/vlc-$VLC_VER-intel64.dmg
   cp -R "/Volumes/VLC media player/VLC.app" ./deps
 fi
