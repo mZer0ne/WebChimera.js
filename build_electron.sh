3df94d5d7ca95c7a4fb68e1554a8356fd82c7035
Oleg Filippov <oleg@polbox.tv>
2022-11-11 11:25:02 +0200

    Downgrade electron

--- build_electron.sh
+++ build_electron.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
-export npm_config_wcjs_runtime_version="19.1.1"
-export npm_config_wcjs_runtime="electron"
+# Default params
+export BUILD_DIR="./build/Release"
+export TRAVIS_OS_NAME="osx"
+export MY_PLATFORM="arm64"
+export TRAVIS="TRUE"
 
+# Versions
+export t="19.1.1"
+export VLC_VER="3.0.17.3"
+export TRAVIS_TAG="1.2.0"
+
+# Other
+export WCJS_FULL_ARCHIVE=WebChimera.js_${TRAVIS_TAG}_electron_v${ELECTRON_VER}_VLC_v${VLC_VER}_${MY_PLATFORM}_${TRAVIS_OS_NAME}.tar.gz
+export WCJS_ARCHIVE=WebChimera.js_electron_v${ELECTRON_VER}_${MY_PLATFORM}_${TRAVIS_OS_NAME}.zip
+export WCJS_FULL_ARCHIVE_PATH=$BUILD_DIR/$WCJS_FULL_ARCHIVE
+export WCJS_ARCHIVE_PATH=$BUILD_DIR/$WCJS_ARCHIVE
+
+./travis_before_install.sh
 npm install
+./travis_before_deploy.sh
--- travis_before_install.sh
+++ travis_before_install.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-if [ ! $TRAVIS ]
-then
-	exit 1
+if [ ! $TRAVIS ]; then
+  exit 1
+fi
+
+if [ ! $MY_PLATFORM ]; then
+  export MY_PLATFORM="intel64"
 fi
 
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-  curl -L -o ./deps/vlc-$VLC_VER-intel64.dmg https://get.videolan.org/vlc/$VLC_VER/macosx/vlc-$VLC_VER-intel64.dmg --insecure
-  hdiutil mount ./deps/vlc-$VLC_VER-intel64.dmg
+  curl -L -o ./deps/vlc-$VLC_VER-$MY_PLATFORM.dmg https://get.videolan.org/vlc/$VLC_VER/macosx/vlc-$VLC_VER-$MY_PLATFORM.dmg --insecure
+  hdiutil mount ./deps/vlc-$VLC_VER-$MY_PLATFORM.dmg
   cp -R "/Volumes/VLC media player/VLC.app" ./deps
 fi
